cdf0c14c941893f7ca71f15356545d326b8b37b5
Nathan-Satt <nathandsatterfield@gmail.com>
2021-04-08 19:34:25 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get  '/about',        to: 'marathon#about'
   get  '/signup',       to: 'users#new'
@@ -9,6 +10,10 @@ Rails.application.routes.draw do
   get  '/home',         to: 'marathon#home'
   get  '/teamsignup',   to: 'marathon#teamsignup'
   get  '/contact',      to: 'marathon#contact'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   root 'marathon#home'
   #root 'application#home'
   resources :users
